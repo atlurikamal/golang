18ba9efa9c81d2ec555c1605defd66ffece86e67
atlurikamal <atlurikamal@mail.fresnostate.edu>
2015-11-08 16:14:28 -0800

    lease enter the commit message for your changes. Lines starting  Changes to be committed: 	new file:   nam.go 	new file:   name.go

---
+++ nam.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+
+	"fmt"
+)
+
+func main() {
+
+var name string ="Kamal Atluri"
+
+fmt.Println("The value of the string is: ",name)
+}
---
+++ name.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+
+	"fmt"
+	"Mynamepkg"
+)
+
+func main() {
+
+fmt.Println(Mynamepkg.Name)
+}
