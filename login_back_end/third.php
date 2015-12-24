d8c812d96f9a12f43662b5f5e68f0ee5f99bdc9a
atlurikamal <atlurikamal@mail.fresnostate.edu>
2015-12-23 16:49:10 -0800

    Your branch is up-to-date with 'origin/master'.
     Changes to be committed:
    	new file:   login_back_end/a.html
    	new file:   login_back_end/app.yaml
    	new file:   login_back_end/b.html
    	new file:   login_back_end/c.html
    	new file:   login_back_end/doc.go
    	new file:   login_back_end/eighth.php
    	new file:   login_back_end/eleventh.php
    	new file:   login_back_end/fifth.php
    	new file:   login_back_end/first.php
    	new file:   login_back_end/fourth.php
    	new file:   login_back_end/kamal.html
    new file:   login_back_end/login.html
    	new file:   login_back_end/main.go
    	new file:   login_back_end/mainhtml.html
    	new file:   login_back_end/ninth.php
    	new file:   login_back_end/second.php
    	new file:   login_back_end/seventh.php
    	new file:   login_back_end/sixth.php
    	new file:   login_back_end/tenth.php
    	new file:   login_back_end/third.php
    	new file:   login_back_end/tweleth.php

---
+++ login_back_end/third.php
@@ -0,0 +1,23 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "kamal";
+$dbname = "kaf";
+
+if(!(mysqli_connect($servername,$username,$password))) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+$sql = "SELECT id, festname FROM fests WHERE month = 'March'";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "id: " . $row["id"]. " - Name: " . $row["firstname"] "<br>";
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
+?>
